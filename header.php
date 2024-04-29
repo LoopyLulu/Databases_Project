30b6f851f0238ce04af13bf2b1d1546ed7573893
JoshuaYu50 <joshyumail@gmail.com>
2024-04-28 23:30:28 -0400

    Added sign up to header

--- header.php
+++ header.php
@@ -11,7 +11,10 @@
         <ul class="navbar-nav ms-auto">
           <!-- check if currently logged in, display Log out button 
                otherwise, display sign up and log in buttons -->
-          <?php if (!isset($_SESSION['username'])) { ?>              
+          <?php if (!isset($_SESSION['username'])) { ?>
+            <li class="nav-item">
+              <a class="nav-link" href="signup.php">Sign up</a>
+            </li>              
             <li class="nav-item">
               <a class="nav-link" href="signin.php">Sign in</a>
             </li>              
