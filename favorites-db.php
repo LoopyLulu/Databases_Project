efb94f503247e05908c0b4dec2ef10dcc012c45a
maksiaeai <98066072+maksiaeai@users.noreply.github.com>
2024-04-28 23:38:50 -0400

    Update favorites-db.php

--- favorites-db.php
+++ favorites-db.php
@@ -1,35 +1,6 @@
 <?php
 require("connect-db.php");
 
-function getFilteredSnacks($excludedAllergens)
-{
-    global $db;
-
-    $query = "SELECT * FROM Project_Snack WHERE snack_ID NOT IN (SELECT SnackID FROM Project_ContainsAllergen WHERE ";
-
-    $conditions = [];
-    foreach ($excludedAllergens as $allergen) {
-        $conditions[] = "$allergen = 1";
-    }
-
-    // Join all conditions with OR (since we want to exclude snacks that have any of these allergens)
-    $query .= implode(' OR ', $conditions);
-
-    $query .= ")";
-
-	echo $query;
-
-    try {
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $result = $statement->fetchAll();
-        $statement->closeCursor();
-        return $result;
-    } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-}
-
 
 function favoriteSnack($userID, $snack_ID) {
    global $db; // Database connection
@@ -43,33 +14,20 @@ function favoriteSnack($userID, $snack_ID) {
 
        $snackID = $db->lastInsertId();
        $statement1->closeCursor();
-       
-       $query2 = "INSERT INTO Project_ContainsAllergen (SnackID) VALUES (:snackID)";
-       $statement2 = $db->prepare($query2);
-       $statement2->bindValue(':snackID', $snackID);
-       $statement2->execute();
-       $statement2->closeCursor();
-
-       
-       foreach ($allergenList as $allergen) {
-           $query3 = "UPDATE Project_ContainsAllergen SET $allergen = 1 WHERE SnackID = :snackID";
-           $statement3 = $db->prepare($query3);
-           $statement3->bindValue(':snackID', $snackID);
-           $statement3->execute();
-           $statement3->closeCursor();
-       }
+      
    } catch (PDOException $e) {
        echo "Error: " . $e->getMessage();
    }
 }
 
-function getAllSnacks()
+function getAllFavorites($userID)
 {
     global $db;
 
     try {
-        $query = "SELECT * FROM Project_Snack";
+        $query = "SELECT * FROM Project_FavoritesTable NATURAL JOIN Project_Snack WHERE userID = :userID";
         $statement = $db->prepare($query);
+		$statement->bindValue(':userID', $userID);
         $statement->execute();
         $result = $statement->fetchAll();
         $statement->closeCursor();
@@ -80,64 +38,7 @@ function getAllSnacks()
     }
 }
 
-function getSnackById($id)
-{
-   global $db;
-   
-   try {
-   $query = "SELECT * FROM Project_Snack WHERE snack_ID=:id";
-   $statement = $db->prepare($query);
-   $statement->bindValue(':id', $id);
-   $statement->execute();
-   $result = $statement->fetch();
-   $statement->closeCursor();
-
-   return $result;
-
-   } catch (PDOException $e) {
-       echo "Error: " . $e->getMessage();
-   }
-}
-
-function updateSnack($snackId, $snackName, $ingredients, $allergens, $allergenList)
-{
-    global $db;
-
-    try {
-    // reset all to 0
-    $query2 = "UPDATE Project_ContainsAllergen SET milk=0, eggs=0, fish=0, shellfish=0, tree_nuts=0, peanuts=0, wheat=0, soybeans=0, sesame=0 WHERE SnackID=:snackId";
-    $statement2 = $db->prepare($query2);
-    $statement2->bindValue(':snackId', $snackId);
-    $statement2->execute();
-    $statement2->closeCursor();
-    
-    $query = "UPDATE Project_Snack SET Sname=:snackName, ingredients=:ingredients, allergens=:allergens WHERE snack_ID=:snackId";
-
-    $statement = $db->prepare($query);
-    $statement->bindValue(':snackId', $snackId);
-    $statement->bindValue(':snackName', $snackName);
-    $statement->bindValue(':ingredients', $ingredients);
-    $statement->bindValue(':allergens', $allergens);
-
-    $statement->execute();
-    $statement->closeCursor();
-
-
-    // re-update the list
-    foreach ($allergenList as $allergen) {
-        $query3 = "UPDATE Project_ContainsAllergen SET $allergen=1 WHERE SnackID=:snackId";
-        $statement3 = $db->prepare($query3);
-        $statement3->bindValue(':snackId', $snackId);
-        $statement3->execute();
-        $statement3->closeCursor();
-    }
-
-   } catch (PDOException $e) {
-       echo "Error: " . $e->getMessage();
-   }
-}
-
-function unfavoriteSnack($snackId)
+function unfavoriteSnack($userID, $snackId)
 {
    global $db;
 
